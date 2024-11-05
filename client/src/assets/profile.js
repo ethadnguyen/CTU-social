11d30f093a23a9e4bb42973228ec2f4b3a3980d8
Neal Nguyen <hluc2003@gmail.com>
2024-11-05 23:13:56 +0700

    profile page - add form

--- client/src/assets/profile.js
+++ client/src/assets/profile.js
@@ -44,7 +44,6 @@ export const profile = {
         adminId: "64df3c064180b81adfe41d4b",
         name: "Hội thánh đức chúa trời",
         description: "Mọi người nhớ donet cho giáo chủ để tích đức online",
-        banner: "/pictures/1 (1).jpg",
       },
     ],
     friends: [
